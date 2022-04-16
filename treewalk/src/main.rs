1409881973d2c8b84b9a5f91bca35f8e5cfa5a75
Andrei Formiga <archimedes_siracusa@hotmail.com>
2022-04-16 19:55:52 -0300

    a bit of the main program

--- treewalk/src/main.rs
+++ treewalk/src/main.rs
@@ -1,3 +1,28 @@
+use std::env;
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Lox interpreter");
+    let args : Vec<String> = env::args().skip(1).collect();
+    if args.len() > 1 {
+        println!("Usage: rlox [filename]");
+        std::process::exit(1);
+    }
+    if args.len() == 1 {
+        println!("Processing file: {}", &args[0]);
+        process_file(&args[0]);
+    } else {
+        println!("Opening the REPL...");
+    }
+}
+
+fn run(contents: &str) {
+    println!("{}", contents);
+}
+
+fn process_file(fname: &str) {
+    match read_to_string(fname) {
+        Ok(s) => run(&s),
+        Err(e) => println!("Error opening file: {}", e)
+    }
 }
